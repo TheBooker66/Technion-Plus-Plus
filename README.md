5f7d91342cf91940631c43132f57e46216a436a3
TheBooker66 <thebooker666@gmail.com>
2025-01-08 19:29:54 +0200

    Modified minifier.ps1 and added a src dir accordingly.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Technion<sup>++</sup>
 כרגע קצת ריק כאן, אבל אולי בעתיד אכתוב כאן עוד. רוב מה שאתם צריכים לדעת נמצא בתיאור של התוסף בחנות התוספים, [כאן](https://chromewebstore.google.com/detail/technion-plus-plus/pfhjnidbfndnjhpcpfecngcigdjebemk).
 אם אתם רוצים לתרום, להלן קוד QR של ביט:
-![](resources/bit_donation.jpg)
+![](src/resources/bit_donation.jpg)
--- minifier.ps1
+++ minifier.ps1
@@ -1,32 +1,71 @@
-# Get all files in the current directory
-$js_files = Get-ChildItem -Path .,.\js\,.\cheesefork\ -Filter "*.js" | Select-Object -ExpandProperty FullName
-$html_files = Get-ChildItem -Path .,.\html\,.\html\templates -Filter "*.html" | Select-Object -ExpandProperty FullName
-$css_files = Get-ChildItem -Path .\css\,.\css\moodle\ -Filter "*.css" | Select-Object -ExpandProperty FullName
-$svg_files = Get-ChildItem -Path .\icons\,.\icons\panopto_icons,.\icons\release_notes -Filter "*.svg" | Select-Object -ExpandProperty FullName
+# Get all files to minify
+$cwd = Join-Path -Path (Get-Location).Path -ChildPath "src"
+$files = Get-ChildItem -Path $cwd -Recurse | Select-Object -ExpandProperty FullName
 
-# Loop through all the files and minify them
-foreach ($file in $js_files)
-{
-    uglifyjs $file -o $file --compress --mangle
-    Write-Host "Minified-js: $( $file )"
+# Delete previously minified folder
+$folderPath = Join-Path -Path (Get-Location).Path -ChildPath "minified"
+if (Test-Path -Path $folderPath -PathType Container) {
+    Remove-Item -Path $folderPath -Recurse -Force
+    Write-Host "Folder '$folderPath' deleted successfully."
 }
-foreach ($file in $html_files)
+
+# Loop through all the files
+foreach ($file in $files)
 {
-    html-minifier --collapse-whitespace --remove-comments --remove-tag-whitespace --use-short-doctype --minify-css true --minify-js true $file -o $file
-    Write-Host "Minified-html: $( $file )"
-}
-foreach ($file in $css_files)
-{
-    cleancss -o $file $file
-    Write-Host "Minified-css: $( $file )"
-}
-foreach ($file in $svg_files)
-{
-    # Skip the webwork and paypal logo svg files, because they are proprietary
-    if ($file -like "*webwork.svg" -or $file -like "*paypal_logo.svg")
+    # Skip the webwork and paypal logo svg files, because they are proprietary,
+    # and the share-histograms javascript file, out of respect for Michael Maltsev
+    if ($file -like "*webwork.svg" -or $file -like "*paypal_logo.svg" -or $file -like "*share-histograms.js")
     {
+        Write-Host "Skipped: $( $file )"
         continue
     }
-    html-minifier --collapse-whitespace --remove-comments --case-sensitive $file -o $file
-    Write-Host "Minified-svg: $( $file )"
+
+    # Figure out where to save the file (the 4 is to remove the "src\" part of the path)
+    $outputPath = Join-Path -Path $folderPath -ChildPath $file.Substring((Get-Location).Path.Length + 4)
+
+    # Check if the file is actually a file or a folder
+    if (Test-Path -Path $file -PathType Leaf)
+    {
+        # Minify all minifiable files and copy all other files
+        switch ((Split-Path -Leaf $file).Split(".")[-1])
+        {
+            "js" {
+                uglifyjs $file -o $outputPath --compress --mangle
+                Write-Host "Minified js: $( $outputPath )"
+            }
+            "html" {
+                html-minifier $file -o $outputPath --collapse-whitespace --remove-comments --remove-tag-whitespace --use-short-doctype --minify-css true --minify-js true
+                Write-Host "Minified html: $( $outputPath )"
+            }
+            "css" {
+                cleancss -o $outputPath $file
+                Write-Host "Minified css: $( $outputPath )"
+            }
+            "svg" {
+                html-minifier $file -o $outputPath --collapse-whitespace --remove-comments --case-sensitive
+                Write-Host "Minified svg: $( $outputPath )"
+            }
+            default {
+                Copy-Item -Path $file -Destination $outputPath
+                Write-Host "Copied Random: $( $outputPath )"
+            }
+        }
+    }
+    else
+    {
+        Copy-Item -Path $file -Destination $outputPath -Recurse
+        Write-Host "Copied folder: $( $outputPath )"
+    }
 }
+
+# Delete previously zipped folder
+$outputPath = $cwd.Replace("\src", "\minified.zip")
+if (Test-Path -Path $outputPath)
+{
+    Remove-Item -Path $outputPath -Force
+    Write-Host "File '$outputPath' deleted successfully."
+}
+
+# Zip the folder
+Compress-Archive -Path $cwd.Replace("\src", "\minified\*") -DestinationPath $outputPath
+Write-Host "Zipped folder: $( $outputPath )"
