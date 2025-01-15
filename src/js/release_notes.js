60d90ca8446bf06befdcc12b38e91e13e385c542
TheBooker66 <thebooker666@gmail.com>
2025-01-15 17:46:57 +0200

    Cleared up date in release notes, css fix and link fix

--- src/js/release_notes.js
+++ src/js/release_notes.js
@@ -1,4 +1,5 @@
 let version_number = chrome.runtime.getManifest().version;
 let date_element = document.getElementById("date").innerText;
-let date = new Date(date_element).toLocaleDateString('he-IL', { year: 'numeric', month: 'long', day: 'numeric' });
-document.getElementById("version").innerText = `מה חדש – גירסה ${version_number.trim()} – ${date.trim()}`;
+let date = new Date(date_element).toLocaleDateString('he-IL', {year: 'numeric', month: 'long', day: 'numeric'});
+document.getElementById("title").innerText = `מה חדש – גירסה ${version_number.trim()} – ${date.trim()}`;
+document.getElementById("version_range").innerHTML = "&#x202A;" + version_number.replace(version_number.split('.').at(-1), '').substring(0, version_number.length - 2) + ".x&#x202C;";
