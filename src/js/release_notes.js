f618c0c812a29e7bf7867ab94c833ad3931591f8
TheBooker66 <thebooker666@gmail.com>
2025-07-16 20:35:19 +0300

    html Cleanup

--- src/js/release_notes.js
+++ src/js/release_notes.js
@@ -7,4 +7,8 @@
 	document.getElementById("title").textContent = `מה חדש – גירסה ${version_number.trim()} – ${date.trim()}`;
 	document.getElementById("version_range").textContent =
 		version_number.substring(0, version_number.indexOf('.', version_number.indexOf('.') + 1));
+	const linkElement = document.getElementById("storeLink"),
+		storeURL = "https://chromewebstore.google.com/detail/technion-plus-plus/pfhjnidbfndnjhpcpfecngcigdjebemk";
+	linkElement.setAttribute("href", storeURL);
+	linkElement.setAttribute("target", "_blank");
 })();
